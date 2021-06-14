0b083a5601fe53738b66d501f855cdba6acd273a
RTumul <58302522+RTumul@users.noreply.github.com>
2021-06-14 10:54:36 +0600

    Update 5.php

--- Lab 3/5.php
+++ Lab 3/5.php
@@ -3,20 +3,21 @@
 	<body>
 		<?php
 $backColor="green"; $fontColor="red"; $imageWidth="80px"; $imageHeight="80px";
+?>
 
-
-echo "<table>
+<table>
 	<tr>
-		<td><img src="aiub_logo.png" width=$imageWidth height=$imageHeight /></td>
-		<td><h2>American International University -<span style="background-color:$backColor;color:$fontColor">
+		<td><img src="aiub_logo.png" width=<?php echo "$imageWidth" ?>; height=<?php echo"$imageHeight"?> /></td>
+		<td><h2>American International <br/>University -<span style="background-color:<?php echo "$backColor";?>;color:<?php echo"$fontColor";?>">
         Bangladesh</h2>
 </span>
 </td>
 		
 	</tr>
-</table>";
-?>
+</table>
+
 
 	</body>
 </html>
+
 
